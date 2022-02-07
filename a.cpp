63a7f199f6fc5bde0bb8050b77442252ffe241f4
Jianend <2110179446@qq.com>
2022-02-07 15:44:01 +0800

    加入幂运算

--- a.cpp
+++ a.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<math.h>
 int calculator(int a,char s ,int b){
     int c;
     if (s=='+')
@@ -9,15 +10,17 @@ else if (s=='*')
 c=a*b;
 else if (s=='/')
 c=a/b;
-
+else if (s=='^')
+ c=pow(a,b);
     return c;
 }
 int main()
 {int c;
-    printf("add-----------1\n");
-    printf("sud-----------2\n");
-    printf("mul-----------3\n");
-    printf("div-----------4\n");
+    printf("|+      1|\n");
+    printf("|-      2|\n");
+    printf("|*      3|\n");
+    printf("|/      4|\n");
+    printf("|^      5|\n");
     char s;
     int a,b;
 scanf("%d%c%d",&a,&s,&b);
